644d9b8f22672c2f8b1fe951293ae1cb9f8cc57d
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2024-07-23 11:47:34 +0100

    Added a new interpreter for the language
    added arithmetic operands and reversal functions based on stack
    
    updated on 23/07/2024

---
+++ interpreter.py
@@ -0,0 +1,43 @@
+from ulto_parser import Assign, Print, Num, Var, BinOp
+
+
+class Interpreter:
+    def __init__(self):
+        self.variables = {}
+        self.history = []
+
+    def interpret(self, nodes):
+        for node in nodes:
+            self.visit(node)
+
+    def visit(self, node):
+        if isinstance(node, Num):
+            return node.value
+        elif isinstance(node, Var):
+            return self.variables.get(node.name, 0)
+        elif isinstance(node, BinOp):
+            left_val = self.visit(node.left)
+            right_val = self.visit(node.right)
+            if node.op == 'PLUS':
+                return left_val + right_val
+            elif node.op == 'MINUS':
+                return left_val - right_val
+            elif node.op == 'MULTIPLY':
+                return left_val * right_val
+            elif node.op == 'DIVIDE':
+                return left_val / right_val
+        elif isinstance(node, Assign):
+            value = self.visit(node.value)
+            self.history.append((node.name, self.variables.get(node.name, None)))
+            self.variables[node.name] = value
+        elif isinstance(node, Print):
+            value = self.visit(node.value)
+            print(value)
+
+    def reverse(self):
+        if self.history:
+            var_name, old_value = self.history.pop()
+            if old_value is None:
+                del self.variables[var_name]
+            else:
+                self.variables[var_name] = old_value
