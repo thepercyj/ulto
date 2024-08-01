4e8f0c82556f10ef54863eacf4bfcdca9c861b11
Aman Thapa Magar (PJ) <aman.yhbs@gmail.com>
2024-08-01 14:34:06 +0100

    added .ul extension support for the language
    - added main.py file by providing support for language which are denoted as .ul
    
    updated on 01/08/2024

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+from lexer import tokenize
+from ulto_parser import Parser
+from execution_engine import ExecutionEngine
+from semantic_analyser import SemanticAnalyser
+
+def main(filename):
+    # Read the code from the file
+    with open(filename, 'r') as file:
+        code = file.read()
+
+    # Tokenize the code
+    tokens = tokenize(code)
+    print("Tokens:", tokens)
+
+    # Parse the tokens into an AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    print("AST:", ast)
+
+    # Perform semantic analysis
+    analyser = SemanticAnalyser(ast)
+    analyser.analyse()
+
+    # Execute the AST
+    engine = ExecutionEngine(ast, analyser.symbol_table)
+    engine.execute()
+
+    # Print the final state of the variables
+    print("Final state of variables:", engine.symbol_table)
+
+if __name__ == '__main__':
+    # Check if the filename is provided
+    if len(sys.argv) != 2:
+        print("Usage: ulto <filename>.ul")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    # Check if the file has the correct extension
+    if not filename.endswith('.ul'):
+        print("Error: The file must have a .ul extension")
+        sys.exit(1)
+
+    main(filename)
